9ad4e634db96c9fab8daf6e7d9b23dc3689eb4b2
Goroshchik Ilya <goooroshchik@gmail.com>
2022-08-16 11:13:38 +0300

    Change enviromental variables and change field in shop model

--- car_shop/shop/models.py
+++ car_shop/shop/models.py
@@ -9,21 +9,21 @@ from django.contrib.auth.models import User
 
 class Shop(Base):
     class Meta:
-        verbose_name = 'Shop'
-        verbose_name_plural = 'Shops'
-        db_table = 'shop'
+        verbose_name = "Shop"
+        verbose_name_plural = "Shops"
+        db_table = "shop"
 
-    first_name = models.CharField(default='', max_length=120)
+    first_name = models.CharField(default="", max_length=120)
     location = fields.CountryField()
     phone_number = models.IntegerField(default=0)
-    cars = models.ManyToManyField(Car, through='ShopCar')
-    car_types = models.CharField()  # TODO
+    cars = models.ManyToManyField(Car, through="ShopCar")
+    car_types = models.JSONField()
     balance = models.IntegerField(default=0)
 
 
 class ShopCar(models.Model):
     class Meta:
-        db_table = 'shop_car'
+        db_table = "shop_car"
 
     price = models.IntegerField(default=0)
     count = models.IntegerField(default=0)
@@ -33,7 +33,7 @@ class ShopCar(models.Model):
 
 class ShopHistory(models.Model):
     class Meta:
-        db_table = 'shop_history'
+        db_table = "shop_history"
 
     date = models.DateTimeField(default=datetime.datetime.now())
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
@@ -43,4 +43,3 @@ class ShopHistory(models.Model):
 class ShopManagerUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     pass
-
